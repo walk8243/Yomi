3b3d3c52b982f147e351f21afa0337f5e11a4691
walk8243 <walk8243@gmail.com>
2025-04-27 03:33:54 +0900

    見た目の調整

--- app/_shogi/gui.tsx
+++ app/_shogi/gui.tsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
 import Board from "./board";
-import { BoardData, Situation } from "./utils/variable";
+import { Situation } from "./utils/variable";
 import styles from "./gui.module.scss";
 import { printSituationHands } from "./utils/parser";
 
 export default function Gui({ kif }: Readonly<{ kif: Situation[] }>) {
-  const [board, setBoard] = useState<BoardData>(kif[0].board);
+  const [selected, setSelected] = useState<number>(0);
 
   return (
     <div className={styles.container}>
-      <Board board={board} />
+      <Board board={kif[selected].board} />
       <ul className={styles.list}>
         {kif.map((situation, index) => (
-          <li className={styles.item} key={situation.hands ? `${index}-${situation.hands.after.x}-${situation.hands.after.y}` : 'init'}>
-            <button onClick={() => { setBoard(situation.board); }}>
-              {printSituationHands(situation.hands)}
+          <li className={`${styles.item} ${selected === index ? styles['item--selected'] : ''}`}
+              key={situation.hands ? `${index}-${situation.hands.after.x}-${situation.hands.after.y}` : 'init'}>
+            <button onClick={() => { setSelected(index); }}>
+              <span className={styles.item__index}>{index}</span>
+              <span className={styles.item__text}>{printSituationHands(situation.hands)}</span>
             </button>
           </li>
         ))}
