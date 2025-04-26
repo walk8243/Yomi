936f478d1ec5f7c43c3dd8a03255bbf6ae30b52a
walk8243 <walk8243@gmail.com>
2025-04-27 02:50:39 +0900

    駒台をダミーで用意

--- app/_shogi/board.tsx
+++ app/_shogi/board.tsx
@@ -1,16 +1,40 @@
 import Piece from './piece';
 import styles from './board.module.scss';
-import { BoardData } from './utils/variable';
+import { BoardData, firstPlayer, fu, gin, hisha, kaku, keima, kin, kyosha, secondPlayer } from './utils/variable';
 
 export default function Board({ board }: Readonly<{ board: BoardData }>) {
   return (
-    <div className={styles.board}>
-      <div className={styles.board__inner}>
-        {board.map((piece, index) => (
-          <div className={styles.board__space} key={piece ? `${index}-${piece.player.description}-${piece.type.description}` : `${index}-empty`}>
-            {piece ? <Piece piece={piece} /> : ''}
-          </div>
-        ))}
+    <div className={styles.container}>
+      <div className={styles.container__second}>
+        <div className={styles.stand}>
+          <Piece piece={{ type: hisha, player: secondPlayer }} />
+          <Piece piece={{ type: kaku, player: secondPlayer }} />
+          <Piece piece={{ type: kyosha, player: secondPlayer }} />
+          <Piece piece={{ type: fu, player: secondPlayer }} />
+          <Piece piece={{ type: fu, player: secondPlayer }} />
+          <Piece piece={{ type: fu, player: secondPlayer }} />
+        </div>
+        <div className={styles.player}>藤井聡太 竜王名人</div>
+      </div>
+      <div className={styles.board}>
+        <div className={styles.board__inner}>
+          {board.map((piece, index) => (
+            <div className={styles.board__square} key={piece ? `${index}-${piece.player.description}-${piece.type.description}` : `${index}-empty`}>
+              {piece ? <Piece piece={piece} /> : ''}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.container__first}>
+        <div className={styles.stand}>
+          <Piece piece={{ type: kin, player: firstPlayer }} />
+          <Piece piece={{ type: kin, player: firstPlayer }} />
+          <Piece piece={{ type: gin, player: firstPlayer }} />
+          <Piece piece={{ type: keima, player: firstPlayer }} />
+          <Piece piece={{ type: keima, player: firstPlayer }} />
+          <Piece piece={{ type: fu, player: firstPlayer }} />
+        </div>
+        <div className={styles.player}>永瀬拓矢 九段</div>
       </div>
     </div>
   );
