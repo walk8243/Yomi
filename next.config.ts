a341f6755c46ee41afe32f01135d86da4225e4b1
walk8243 <walk8243@gmail.com>
2025-05-09 02:02:35 +0900

    試しにNext.jsの設定にpath設定を入れてみる

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,8 @@ const nextConfig = (phase: string, { defaultConfig }: { defaultConfig: NextConfi
     images: {
       unoptimized: true,
     },
+    assetPrefix: process.env.ASSET_PREFIX,
+    basePath: process.env.BASE_PATH,
   }
 };
 
